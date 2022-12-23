c74e9de0be2966c31762cfb057eb5231c2f642a4
infinity1729 <anant.bansal2003@gmail.com>
2022-12-23 12:39:07 +0530

    documented code and added sharpe,EF allocation

--- Portfolio_Optimisation.py
+++ Portfolio_Optimisation.py
@@ -6,17 +6,27 @@ import matplotlib.pyplot as plt
 import streamlit as st
 
 def portfolio_annualised_performance(weights, mean_returns, cov_matrix):
+    '''
+    Gives standard deviation, returns calculated for the portfolio with given weights and mean_returns.
+    '''
     returns = np.sum(mean_returns*weights ) *252
     std = np.sqrt(np.dot(weights.T, np.dot(cov_matrix, weights))) * np.sqrt(252)
     return std, returns
 
 
 def neg_sharpe_ratio(weights, mean_returns, cov_matrix, risk_free_rate):
+    '''
+    Negative Sharpe Ratio: calculated because to maximize the sharpe ration we are using scipy.optimize.minimize method. Therefore maximizing our sharpe ratio is equivalent to minimizing negative sharpe ration.
+    '''
     p_var, p_ret = portfolio_annualised_performance(weights, mean_returns, cov_matrix)
     return -(p_ret - risk_free_rate) / p_var
 
 
 def max_sharpe_ratio(mean_returns, cov_matrix, risk_free_rate):
+    '''
+    Based on calculated returns, covariance matrix and provided risk free rate, it returns the weights which provides the maximum sharpe ratio.
+    The algorithm used for minimizing negative sharpe ratio (maximizing sharpe ratio) is Sequential Least Squares Programming (SLSQP) with the bounds on weights having value in the range (0,1) and constraints that sum of weights = 1.
+    '''
     num_assets = len(mean_returns)
     args = (mean_returns, cov_matrix, risk_free_rate)
     constraints = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
@@ -26,21 +36,29 @@ def max_sharpe_ratio(mean_returns, cov_matrix, risk_free_rate):
     return result
 
 def portfolio_volatility(weights, mean_returns, cov_matrix):
+    '''
+    Gives standard deviation of returns over a time horizon of 1 year.
+    '''
     return portfolio_annualised_performance(weights, mean_returns, cov_matrix)[0]
 
 def min_variance(mean_returns, cov_matrix):
+    '''
+    Based on calculated returns and covariance matrix, it returns the weights which provides the minimum portfolio volatility.
+    The algorithm used for minimizing variance is Sequential Least Squares Programming (SLSQP) with the bounds on weights having value in the range (0,1) and constraints that sum of weights = 1.
+    '''
     num_assets = len(mean_returns)
     args = (mean_returns, cov_matrix)
     constraints = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
     bound = (0.0,1.0)
     bounds = tuple(bound for asset in range(num_assets))
-
-    result = sco.minimize(portfolio_volatility, num_assets*[1./num_assets,], args=args,
-                        method='SLSQP', bounds=bounds, constraints=constraints)
-
+    result = sco.minimize(portfolio_volatility, num_assets*[1./num_assets,], args=args,method='SLSQP', bounds=bounds, constraints=constraints)
     return result
 
 def efficient_return(mean_returns, cov_matrix, target):
+    '''
+    Based on calculated returns, covariance matrix and provided target returns it returns the weights which provides the minimum portfolio volatility (measured risk as considered by Harry Markowitz).
+    The algorithm used for minimizing variance is Sequential Least Squares Programming (SLSQP) with the bounds on weights having value in the range (0,1) and constraints that sum of weights = 1 and portfolio returns for current allocated weights is equal to target returns.
+    '''
     num_assets = len(mean_returns)
     args = (mean_returns, cov_matrix)
 
@@ -68,7 +86,7 @@ def display_calculated_ef_with_random(stockList,mean_returns, cov_matrix, risk_f
     
 
     min_vol = min_variance(mean_returns, cov_matrix)
-    sdp_min, rp_min = portfolio_annualised_performance(min_vol['x'], mean_returns, cov_matrix)
+    # sdp_min, rp_min = portfolio_annualised_performance(min_vol['x'], mean_returns, cov_matrix)
     min_vol_allocation = pd.DataFrame(min_vol.x,index=stockList,columns=['allocation'])
     min_vol_allocation.allocation = [round(i*100,2)for i in min_vol_allocation.allocation]
     min_vol_allocation = min_vol_allocation.T
--- main.py
+++ main.py
@@ -3,7 +3,15 @@ import yfinance as yf
 import pandas as pd
 from Portfolio_Optimisation import *
 import streamlit as st
+from streamlit import session_state as ss
 import math
+
+if 'initial_allocate_bool' not in ss:
+	ss.initial_allocate_bool=0
+def set_bool(key,value):
+	ss[key]=value
+
+
 df=pd.read_csv('new_Tickers.csv')
 countries=list(set(df['Country']))
 countries.sort()
@@ -25,39 +33,49 @@ def calculations(stockList,period,risk_free_rate=0):
 		return
 	flag=0
 	for i in df.columns:
-		if math.isnan(df.loc[df.index[0],i]):
+		if math.isnan(df.loc[df.index[1],i]):
 			flag=1
-			print("Data not available for this period for the ticker "+str(i))
+			st.write("Data not available for this period for the ticker "+str(i))
 	if flag==1:
 		exit(1)
 	returns=df.pct_change()
-	print(tickers.history(period=period+"y"))
-	print(returns)
 	mean_returns = returns.mean()
 	cov_matrix = returns.cov()
 	risk_free_rate = risk_free_rate
 	return stockList,mean_returns, cov_matrix, risk_free_rate
+
 def allocation(stockList,period,risk_free_rate=0):
 	stockList,mean_returns, cov_matrix, risk_free_rate=calculations(stockList,period,risk_free_rate)
 	return display_calculated_ef_with_random(stockList,mean_returns, cov_matrix, risk_free_rate)
-# def efficient_frontier_allocate(stockList,period,risk,risk_free_rate=0):
-# 	_,mean_returns,cov_matrix,_=calculations(stockList,period,risk_free_rate)
-# 	max_returns=efficient_frontier(mean_returns,cov_matrix,[risk])
-    
 
-numberOfStocks=st.number_input('Number of Tickers',min_value=2)
-risk_free_rate=st.number_input('Risk free rate')
-period=str(st.number_input("Period of analysis (Eg input value 2 represents 2 years from now",min_value=1))
-country=st.selectbox('Choose Region of Interest',countries)
+def efficient_frontier_allocate(stockList,period,target,risk_free_rate=0):
+	_,mean_returns,cov_matrix,_=calculations(stockList,period,risk_free_rate)
+	max_returns=efficient_return(mean_returns,cov_matrix,target)
+	max_returns_allocation = pd.DataFrame(max_returns.x,index=stockList,columns=['allocation'])
+	max_returns_allocation.allocation = [round(i*100,2)for i in max_returns_allocation.allocation]
+	max_returns_allocation = max_returns_allocation.T
+	return max_returns_allocation, max_returns.fun
+
+numberOfStocks=st.number_input('Number of Tickers',min_value=2,on_change=set_bool, args=('initial_allocate_bool',0))
+risk_free_rate=st.number_input('Risk free rate',on_change=set_bool, args=('initial_allocate_bool',0))
+period=str(st.number_input("Period of analysis (Eg input value 2 represents 2 years from now)",min_value=1,on_change=set_bool, args=('initial_allocate_bool',0)))
+country=st.selectbox('Choose Region of Interest',countries,on_change=set_bool, args=('initial_allocate_bool',0))
 stockList=[]
 for i in range(numberOfStocks):
-	stockList.append(st.selectbox('Choose Ticker {}'.format(i+1),tickerList[country],key=i).split('-')[-1])
-if st.button("Allocate"):
+	stockList.append(st.selectbox('Choose Ticker {}'.format(i+1),tickerList[country]).split('-')[-1])
+stockList.sort()
+allocate=st.button("Allocate", key="initial_allocate",on_click=set_bool, args=('initial_allocate_bool',1))
+if ss.initial_allocate_bool:
 	min_vol_allocation,max_sharpe_allocation=allocation(stockList,period,risk_free_rate)
+	st.write("Minimum Volatility(≈Risk) Allocation")
 	st.table(min_vol_allocation)
-	# risk=st.number_input('Risk you can take')
-	# if st.button("Efficient Frontier Allocation"):
-	# 	st.table(efficient_frontier_allocate(stockList,period,risk_free_rate,risk))
+	st.write("Maximum Sharpe Allocation")
+	st.table(max_sharpe_allocation)
 
-	
+	target=st.number_input('Required returns')
+	if st.button("Efficient Frontier Allocation",on_click=set_bool, args=('initial_allocate_bool',1)):
+		max_returns_allocation, volatility=efficient_frontier_allocate(stockList,period,risk_free_rate,target)
+		st.write("Efficient Frontier Allocation")
+		st.table(max_returns_allocation)
+		st.write("with standard deviation"+str(round(volatility/math.sqrt(252)*100,2))+"%")
 
